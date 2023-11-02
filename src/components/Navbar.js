5567bb913ae2aa9953d435d530e9a4a21f3e9f75
heartbreathing <li.wang@hyperisland.se>
2023-11-02 10:55:49 +0100

    change colors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-500 p-4">
+    <nav className="bg-current p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-white text-2xl font-semibold">Home</Link>
         <div>
-          <Link to="/about" className="text-white ml-4">About</Link>
           <Link to="/products" className="text-white ml-4">Products</Link>
+          <Link to="/about" className="text-white ml-4">About</Link>
+          
         </div>
       </div>
     </nav>
